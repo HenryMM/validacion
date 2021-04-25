b641530486e1dc0fae14533c9fc16c3f3d5f3df0
unknown <henfismat@gmail.com>
2021-04-24 23:37:49 -0500

    validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ValidacionComponent } from './validacion/validacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValidacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/validacion/validacion.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators,FormBuilder } from '@angular/forms';
+import {debounceTime} from 'rxjs/operators';
+@Component({
+  selector: 'app-validacion',
+  templateUrl: './validacion.component.html',
+  styleUrls: ['./validacion.component.css']
+})
+export class ValidacionComponent implements OnInit {
+
+  form: FormGroup;
+  estado:string='';
+  mostrarMensaje:boolean=false;
+
+  rangoSuperior:string;
+  rangoInferior:string;
+
+  constructor() { 
+    this.rangoInferior='2000';
+    this.rangoSuperior='5000';
+    this.buildForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  private buildForm() {
+    this.form = new FormGroup({
+      rangoSuperior: new FormControl(this.rangoSuperior, [Validators.required]),
+      rangoInferior: new FormControl(this.rangoInferior, [Validators.required]),
+      precio: new FormControl('', [Validators.required])
+    });
+
+    this.form.valueChanges
+    .pipe(
+      debounceTime(500)
+    )
+    .subscribe(value => {
+      console.log(value);
+    });
+  }
+
+  save(event: Event) {
+    event.preventDefault();
+    const value = this.form.value;
+    this.mostrarMensaje=true;
+    this.validarPrecio(value);
+    
+    this.rangoSuperior=value.rangoSuperior;
+    this.rangoInferior=value.rangoInferior;
+  }
+
+  validarPrecio(value){
+
+    //El rango superior no puede ser menor al rango inferior
+    if(value.rangoSuperior<=value.rangoInferior){
+      this.estado='ERROR';
+    }else{
+      if(value.precio>=value.rangoSuperior){
+        this.estado='ALTO';
+       }else if(value.precio<value.rangoInferior){
+         this.estado='BAJO';
+       }else{
+         this.estado='MEDIO';
+       }
+    }
+
+  console.log("validarPrecio");
+
+    
+  }
+}
